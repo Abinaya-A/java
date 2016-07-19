f27cd3ea471990be76066850d905e99a2e901b07
Abinaya-A <aabinaya02@gmail.com>
2016-07-19 11:01:30 +0530

    Add files via upload

---
+++ Reg.java
@@ -0,0 +1,17 @@
+public class Reg 
+	{
+	public static void main(String[] args)
+	{
+	int[] num=new int[]{1,2,4,5,7,4,8};
+	for(int i=0;i<num.length;i++)
+	{
+		for(int j=i+1;j<num.length;j++)
+		{
+			if((num[i])==(num[j]))
+			{
+				System.out.println(num[i]);
+			}
+		}
+	}
+}
+}
